05aa9a54fddd49e1adeb89f1c830105439693ee9
alsaco018 <albertoasc018@hotmail.com>
2019-10-04 00:00:08 +0200

    Por fin funciona el correo

--- registro.php
+++ registro.php
@@ -23,8 +23,22 @@
   <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
     <?php
-
-         $servername = "sql104.phpzilla.net";
+    
+            use PHPMailer\PHPMailer\PHPMailer;
+            use PHPMailer\PHPMailer\Exception;
+        
+        /* Exception class. */
+        require 'PHPMailer/src/Exception.php';
+        
+        /* The main PHPMailer class. */
+        require 'PHPMailer/src/PHPMailer.php';
+        
+        /* SMTP class, needed if you want to use SMTP. */
+        require 'PHPMailer/src/SMTP.php';
+        
+        $mailer = new PHPMailer(TRUE);
+        
+        /* $servername = "sql104.phpzilla.net";
          $username = "phpz_24535379";
          $password = "colossusmark45";
          $dbname = "phpz_24535379_diw";
@@ -53,9 +67,105 @@
          mysqli_query($conexion,$sql)
          or die("Problemas en el insert".mysqli_error($conexion));
          mysqli_close($conexion);
-         
+        */
+         try {
+          /* Set the mail sender. */
+          $mailer->setFrom('albertosaldanadiw@gmail.com', 'Alberto');
+       
+          /* Add a recipient. */
+          $mailer->addAddress('alsaco182115@gmail.com', 'Usuario');
+       
+          /* Set the subject. */
+          $mailer->Subject = 'Force';
+       
+          /* Set the mail message body. */
+          $mailer->Body = 'There is a great disturbance in the Force.';
+
+          $mailer->isSMTP();
+          $mailer->Host = 'smtp.gmail.com';
+          $mailer->Port = 587;
+          $mailer->SMTPAuth = true;
+          $mailer->SMTPSecure = 'tls';
+
+          /* Username (email address). */
+          $mailer->Username = 'albertosaldanadiw@gmail.com';
+
+          /* Google account password. */
+          $mailer->Password = 'colossusmark45';
+                
+          /* Finally send the mail. */
+          $mailer->send();
+       }
+       catch (Exception $e)
+       {
+          /* PHPMailer exception. */
+         echo $e->errorMessage();
+       }
+       catch (\Exception $e)
+       {
+          /* PHP exception (note the backslash to select the global namespace Exception class). */
+          echo $e->getMessage();
+       }
+//Import PHPMailer classes into the global namespace
+/*
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Enable SMTP debugging
+// SMTP::DEBUG_OFF = off (for production use)
+// SMTP::DEBUG_CLIENT = client messages
+// SMTP::DEBUG_SERVER = client and server messages
+//$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+//Set the encryption mechanism to use - STARTTLS or SMTPS
+//$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "albertosaldanadiw@gmail.com";
+//Password to use for SMTP authentication
+$mail->Password = "colossusmark45";
+//Set who the message is to be sent from
+$mail->setFrom('albertosaldanadiw@gmail.com', 'First Last');
+//Set an alternative reply-to address
+//$mail->addReplyTo('replyto@example.com', 'First Last');
+//Set who the message is to be sent to
+$mail->addAddress('alsaco182115@gmail.com', 'John Doe');
+//Set the subject line
+$mail->Subject = 'PHPMailer GMail SMTP test';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+//$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+//Replace the plain text body with one created manually
+//$mail->AltBody = 'This is a plain-text message body';
+//Attach an image file
+//$mail->addAttachment('images/phpmailer_mini.png');
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+    //Section 2: IMAP
+    //Uncomment these to save your message in the 'Sent Mail' folder.
+    #if (save_mail($mail)) {
+    #    echo "Message saved!";
+    #}
+}
+
+
+       
+        /*$from = "albertosaldanadiw@gmail.com";
         $to = "alsaco182115@gmail.com"; //email al que enviamos el correo
-        $from = $email; //
+        //$from = $email; //
+        //$to = $_REQUEST['email'];
         $subject = "Registro";
        
         $message = $nick . " Has sido dado de alta en nuestra web:";
@@ -63,12 +173,16 @@
 
         $headers = "From:" . $to;
         $headers2 = "From:" . $to;
-        $envio = mail($to,$subject,$message);
-        $envio2 = mail($from,$subject,$message); // sends a copy of the message to the sender
+        mail($to,$subject,$message,$headers);
+        //$envio2 = mail($from,$subject,$message); // sends a copy of the message to the sender
         
         // You can also use header('Location: thank_you.php'); to redirect to another page.
+    */
     
+
     ?>
+
+
 
 </body>
 </html>
